c857c1579933bcb412eb28a71071230206b5a72f
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-07-12 16:42:27 +0200

    Initial commit

---
+++ SerialTest.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <serial/serial.h>
+
+void enumerate_ports()
+{
+	std::vector<serial::PortInfo> devices_found = serial::list_ports();
+	std::vector<serial::PortInfo>::iterator iter = devices_found.begin();
+
+	while( iter != devices_found.end() )
+	{
+		serial::PortInfo device = *iter++;
+
+		printf( "(%s, %s, %s)\n", device.port.c_str(), device.description.c_str(), device.hardware_id.c_str() );
+	}
+}
+
+int main(int argc, char** argv )
+{
+    enumerate_ports();
+
+    return 0;
+}
